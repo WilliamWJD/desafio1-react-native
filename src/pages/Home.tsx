ff025ad8d8b4990118d05b7e6b95189a470a0f5f
williamwjd <william.jose.wjd@gmail.com>
2022-03-21 20:05:07 -0300

    finalizando desáfio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,32 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const tasksUpdated = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          done: true
+        }
+      }
+      return task
+    })
+
+    setTasks(tasksUpdated)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const tasksUpdated = tasks.filter(task => task.id !== id);
+    setTasks(tasksUpdated)
   }
 
   return (
@@ -26,10 +43,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
